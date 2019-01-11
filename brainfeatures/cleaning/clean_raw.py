c00f234a443eed49876fbb1c9a3b3a917f27cb3f
Lukas Alexander Wilhelm Gemein <gemeinl@informatik.uni-freiburg.de>
2019-01-11 14:57:04 +0100

    now returning pandas data frames with channel names as index. added default cleaning params

--- brainfeatures/cleaning/clean_raw.py
+++ brainfeatures/cleaning/clean_raw.py
@@ -1,14 +1,29 @@
+import pandas as pd
+import numpy as np
+
 from brainfeatures.cleaning.rules import remove_start, remove_end, \
     take_part, resample, clip_values
 
 
-def clean_one_file(signals, fs, sec_to_cut_start, sec_to_cut_end,
+default_cleaning_params = {
+    "sec_to_cut_start": 60,
+    "sec_to_cut_end": 0,
+    "duration_recording_mins": 20,
+    "resample_freq": 100,
+    "max_abs_val": 800,
+    "clip_before_resample": False
+}
+
+
+def clean_one_file(signals, fs, target, sec_to_cut_start, sec_to_cut_end,
                    duration_recording_mins, resample_freq, max_abs_val,
                    clip_before_resample=False):
-    # discard first 60 seconds
+    channels = signals.index
+    signals = np.array(signals)
+    # discard first seconds
     if sec_to_cut_start:
         signals = remove_start(signals, sec_to_cut_start, fs)
-    # discard last 60 seconds
+    # discard last seconds
     if sec_to_cut_end:
         signals = remove_end(signals, sec_to_cut_end, fs)
     # take at most specied number of remaining minutes
@@ -29,4 +44,4 @@ def clean_one_file(signals, fs, sec_to_cut_start, sec_to_cut_end,
         # clip values if desired
         if max_abs_val:
             signals = clip_values(signals, max_abs_val)
-    return signals, resample_freq
+    return pd.DataFrame(signals, index=channels), resample_freq, target
