dc3a1dad34cbf7367586b12936175afbda8cc36d
Lukas Alexander Wilhelm Gemein <gemeinl@informatik.uni-freiburg.de>
2019-01-15 17:21:38 +0100

    renamed 'cleaning' to 'preprocessing'

---
+++ brainfeatures/preprocessing/preprocess_raw.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+
+from brainfeatures.cleaning.rules import remove_start, remove_end, \
+    take_part, resample, clip_values
+
+
+default_preproc_params = {
+    "sec_to_cut_start": 60,
+    "sec_to_cut_end": 0,
+    "duration_recording_mins": 20,
+    "resample_freq": 100,
+    "max_abs_val": 800,
+    "clip_before_resample": False
+}
+
+
+def preprocess_one_file(signals, fs, target, sec_to_cut_start, sec_to_cut_end,
+                        duration_recording_mins, resample_freq, max_abs_val,
+                        clip_before_resample=False):
+    channels = signals.index
+    signals = np.array(signals)
+    # discard first seconds
+    if sec_to_cut_start:
+        signals = remove_start(signals, sec_to_cut_start, fs)
+    # discard last seconds
+    if sec_to_cut_end:
+        signals = remove_end(signals, sec_to_cut_end, fs)
+    # take at most specied number of remaining minutes
+    if duration_recording_mins:
+        signals = take_part(signals, duration_recording_mins, fs)
+
+    if clip_before_resample:
+        # clip values if desired
+        if max_abs_val:
+            signals = clip_values(signals, max_abs_val)
+        # resample if necessary
+        if fs != resample_freq:
+            signals = resample(signals, fs, resample_freq)
+    else:
+        # resample if necessary
+        if fs != resample_freq:
+            signals = resample(signals, fs, resample_freq)
+        # clip values if desired
+        if max_abs_val:
+            signals = clip_values(signals, max_abs_val)
+    return pd.DataFrame(signals, index=channels), resample_freq, target
