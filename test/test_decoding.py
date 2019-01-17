94a68866fa69ed94117ab1f84c7b5b1fa6eb3465
Lukas Alexander Wilhelm Gemein <gemeinl@informatik.uni-freiburg.de>
2019-01-17 16:19:01 +0100

    adapted to recent api changes

--- test/test_decoding.py
+++ test/test_decoding.py
@@ -1,8 +1,9 @@
-import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+import numpy as np
 
 from brainfeatures.decoding.decode import validate, final_evaluate, \
-    get_train_test, get_cropped_train_test, decode_once, apply_pca, apply_scaler
+    get_cropped_train_test, decode_once, apply_pca, apply_scaler, get_train_test
 
 
 def test_validate():
@@ -15,61 +16,90 @@ def test_validate():
     train_ind = [0, 1, 2, 3, 4]
     test_ind = [5, 6, 7, 8, 9]
     clf.fit(X[train_ind], y[train_ind])
-    probas = clf.predict_proba(X[test_ind])
-    expected = np.array(probas)[:, -1]
-
-    result, info = validate(X, y, clf, 2, False, None, None)
-    preds = result["valid"]
-    y_pred = preds[preds.id == 0]["y_pred"]
-    np.testing.assert_array_equal(y_pred, expected)
+    probas1 = clf.predict_proba(X[test_ind])
+    expected_1 = np.array(probas1)[:, -1]
 
     clf.fit(X[test_ind], y[test_ind])
-    probas = clf.predict_proba(X[train_ind])
-    expected = np.array(probas)[:, -1]
+    probas2 = clf.predict_proba(X[train_ind])
+    expected_2 = np.array(probas2)[:, -1]
+
+    X = X[:, np.newaxis]
+    result, info = validate(X, y, clf, 2, shuffle_splits=False,
+                            scaler=None, pca_thresh=None)
+    preds = result["valid"]
+    y_pred = preds[preds.id == 0]["y_pred"]
+    np.testing.assert_array_equal(y_pred, expected_1)
 
     y_pred = preds[preds.id == 1]["y_pred"]
-    np.testing.assert_array_equal(y_pred, expected)
+    np.testing.assert_array_equal(y_pred, expected_2)
 
-    assert "feature_importances" in info["valid"][0]
-
-
-# def test_validate_cropped():
-#     pass
+    assert "feature_importances" in info["valid"]
+    assert "rfpimp_importances" in info["valid"]
 
 
 def test_apply_scaler():
-    from sklearn.preprocessing import StandardScaler
-    scaler = StandardScaler()
     X_train = np.random.rand(10*10).reshape(10, 10)
     X_test = np.random.rand(10*10).reshape(10, 10)
-    result1, result2 = apply_scaler(X_train, X_test, scaler)
-    np.testing.assert_allclose(np.mean(result1), [0], rtol=1e-07, atol=1e-07)
-    np.testing.assert_allclose(np.var(result1), [1], rtol=1e-07, atol=1e-07)
+    X_train = pd.DataFrame(X_train, columns=["a", "b", "c", "d", "e",
+                                             "f", "g", "h", "i", "j"])
+    X_test = pd.DataFrame(X_test, columns=["a", "b", "c", "d", "e",
+                                           "f", "g", "h", "i", "j"])
+    X_train_scaled, X_test_scaled = apply_scaler(X_train, X_test)
+    np.testing.assert_allclose(np.array(X_train_scaled).mean(), [0],
+                               rtol=1e-07, atol=1e-07)
+    np.testing.assert_allclose(np.array(X_train_scaled).var(), [1],
+                               rtol=1e-07, atol=1e-07)
 
 
 def test_apply_pca():
-    pca_thresh = .9
+    pca_thresh = 3
     X_train = np.random.rand(10*10).reshape(10, 10)
     X_test = np.random.rand(10*10).reshape(10, 10)
-    result1, result2, dict_with_pca = apply_pca(X_train, X_test, pca_thresh)
-    # TODO: what to test here?
+    X_train = pd.DataFrame(X_train, columns=["a", "b", "c", "d", "e",
+                                             "f", "g", "h", "i", "j"])
+    X_test = pd.DataFrame(X_test, columns=["a", "b", "c", "d", "e",
+                                           "f", "g", "h", "i", "j"])
+    X_train_pc, X_test_pc, components = apply_pca(X_train, X_test, pca_thresh)
+    assert len(components) == pca_thresh
+    assert X_train_pc.shape[-1] == pca_thresh
+    assert X_test_pc.shape[-1] == pca_thresh
 
 
 def test_get_train_test():
+    # 10 examples, 1 window, 10 features
     X = np.random.rand(10*10).reshape(10, 10)
     y = np.array(5 * [0] + 5 * [1])
     train_ind = [0, 3, 1, 2, 4, 7, 6, 5]
     test_ind = [9, 8]
-    X_train, y_train, X_test, y_test = get_train_test(X, y, train_ind, test_ind)
+
+    X_train, y_train, X_test, y_test = \
+        get_train_test(X, y, train_ind, test_ind)
     np.testing.assert_array_equal(X[train_ind], X_train)
     np.testing.assert_array_equal(y[train_ind], y_train)
     np.testing.assert_array_equal(X[test_ind], X_test)
     np.testing.assert_array_equal(y[test_ind], y_test)
     assert y_train[4] == 0
     assert y_train[-1] == 1
+    # in this case we are not actually doing cropped decoding
+    # every trial is in its own group
+    X = X[:, np.newaxis, :]
+    epoch_to_group_map = []
+    for i, x in enumerate(X):
+        epoch_to_group_map.extend(len(x) * [i])
+    assert len(epoch_to_group_map) == len(X)
+
+    X_train, y_train, X_test, y_test, train_groups, test_groups = \
+        get_cropped_train_test(X, y, train_ind, test_ind, epoch_to_group_map)
+    np.testing.assert_array_equal(X[train_ind].squeeze(), X_train)
+    np.testing.assert_array_equal(X[test_ind].squeeze(), X_test)
+    np.testing.assert_array_equal(y[train_ind], y_train)
+    np.testing.assert_array_equal(y[test_ind], y_test)
+    assert y_train[4] == 0
+    assert y_train[-1] == 1
 
 
 def test_get_cropped_train_test():
+    # 10 examples, 9 windows, 8 features
     X = np.random.rand(10*9*8).reshape(10, 9, 8)
     epoch_to_group_map = []
     for i, x in enumerate(X):
@@ -77,13 +107,14 @@ def test_get_cropped_train_test():
     y = np.array(7 * [0] + 3 * [1])
     train_ind = [0, 1, 2, 3, 4, 5, 6, 7]
     test_ind = [8, 9]
-    X_train, y_train, X_test, y_test, train_groups, test_groups = get_cropped_train_test(
-        X, y, train_ind, test_ind, epoch_to_group_map)
+    X_train, y_train, X_test, y_test, train_groups, test_groups = \
+        get_cropped_train_test(X, y, train_ind, test_ind, epoch_to_group_map)
     expected = len(X[-2]) * [test_ind[-2]] + len(X[-1]) * [test_ind[-1]]
     np.testing.assert_array_equal(expected, test_groups)
     assert len(y_train) == 8 * 9
     assert y_train[7*9] == 1
     assert y_train[7*9-1] == 0
+    assert len(X_test.shape) == 2
 
 
 def test_decode_once():
@@ -99,6 +130,12 @@ def test_decode_once():
     probas = clf.predict_proba(X[test_ind])
     expected = probas[:, 1]
 
-    result_train, result, info = decode_once(X[train_ind], X[test_ind], y[train_ind], clf, scaler=None, pca_thresh=None)
+    X_train = pd.DataFrame(X[train_ind], columns=["a", "b", "c", "d", "e",
+                                                  "f", "g", "h", "i", "j"])
+    X_test = pd.DataFrame(X[test_ind], columns=["a", "b", "c", "d", "e",
+                                                "f", "g", "h", "i", "j"])
+    result_train, result, info = decode_once(
+        X_train, X_test, y[train_ind], y[test_ind],
+        clf, scaler=None, pca_thresh=None)
     np.testing.assert_array_equal(expected, result)
     assert "feature_importances" in info
